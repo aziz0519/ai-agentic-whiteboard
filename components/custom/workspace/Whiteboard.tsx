c3b02cde87c5cc20c392238ba67b8718093d5251
aziz0519 <aziz2834@gmail.com>
2026-09-08 14:31:49 +0800

    feat(whiteboard):added Excelidraw API

--- components/custom/workspace/Whiteboard.tsx
+++ components/custom/workspace/Whiteboard.tsx
@@ -1,12 +1,22 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { Excalidraw } from "@excalidraw/excalidraw"
 import "@excalidraw/excalidraw/index.css"
 
+
 function Whiteboard() {
+
+  const [excalidrawAPI, setExcalidrawAPI] = useState(null);
+  const handleCanvasChange =(elements:readonly any[], appState:any)=>{
+    console.log(appState);
+  };
+
   return (
     <div style={{height: "90vh"}}>
-      <Excalidraw />
+      <Excalidraw
+      //@ts-ignore
+      excalidrawAPI={(api)=>setExcalidrawAPI(api)} 
+      onChange={handleCanvasChange} />
     </div>
   )
 }
